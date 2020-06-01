e40c67507adb74e7a92e20fb648d16cda0487367
Oleksandr Klochko <utoyvo@protonmail.com>
2020-06-01 15:27:31 +0300

    bounce title

--- public/assets/js/bounce.js
+++ public/assets/js/bounce.js
@@ -7,39 +7,42 @@
 			container: $(this).parent(),
 			bumpEdge: function () {}
 		}, options);
-		
+
 		return this.each(function () {
 			var containerWidth, containerHeight, elWidth, elHeight, move, getSizes,
 				$el = $(this);
 
 			getSizes = function () {
-				containerWidth = settings.container.outerWidth();
+				containerWidth  = settings.container.outerWidth();
 				containerHeight = settings.container.outerHeight();
-				elWidth = $el.outerWidth();
+				elWidth  = $el.outerWidth();
 				elHeight = $el.outerHeight();
 			};
 
 			move = {
 				right: function () {
-					$el.animate({left: (containerWidth - elWidth)}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+					$el.animate({left: (containerWidth - elWidth)}, {duration: ((containerWidth / settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
 						settings.bumpEdge();
 						move.left();
 					}});
 				},
+
 				left: function () {
-					$el.animate({left: 0}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+					$el.animate({left: 0}, {duration: ((containerWidth / settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
 						settings.bumpEdge();
 						move.right();
 					}});
 				},
+
 				down: function () {
-					$el.animate({top: (containerHeight - elHeight)}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+					$el.animate({top: (containerHeight - elHeight)}, {duration: ((containerHeight / settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
 						settings.bumpEdge();
 						move.up();
 					}});
 				},
+
 				up: function () {
-					$el.animate({top: 0}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
+					$el.animate({top: 0}, {duration: ((containerHeight / settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
 						settings.bumpEdge();
 						move.down();
 					}});
@@ -55,10 +58,10 @@
 				move.down();
 			}
 
-      // Make that shit responsive!
-      $(window).resize( function() {
-        getSizes();
-      });
+			// Make that shit responsive!
+			$(window).resize( function() {
+				getSizes();
+			});
 		});
 	};
 })(jQuery, window);
@@ -68,8 +71,9 @@ $(document).ready( function() {
 	$('.marquee').marqueeify({
 		speed: 250,
 		bumpEdge: function () {
-			var newColor = "hsl(" + Math.floor(Math.random()*360) + ", 100%, 50%)";
+			var newColor = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
 			$('.marquee .logo').css('fill', newColor);
 		}
 	});
+
 });
